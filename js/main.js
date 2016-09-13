cac7b216f64c08aec63bfd6875965e620bd40d48
loreguerra <lguerra@cid-inc.com>
2016-09-13 10:30:32 -0700

    linted js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-function copyToClipboard(element){
-  //create hidden container
-  var temp = document.createElement("input");
-  //assign value of element to container
-  temp.setAttribute("value", document.getElementById(element).innerHTML);
-  //append it to body
+function copyToClipboard(element) {
+  // create hidden container
+  const temp = document.createElement('input');
+  // assign value of element to container
+  temp.setAttribute('value', document.getElementById(element).innerHTML);
+  // append it to body
   document.body.appendChild(temp);
-  //highlight content
+  // highlight content
   temp.select();
-  //execute copy command
-  document.execCommand("copy");
-  //remove it from body
+  // execute copy command
+  document.execCommand('copy');
+  // remove it from body
   document.body.removeChild(temp);
 }
